0482b857ffbc3709dd57376849386211b2be06b0
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2022-03-06 09:01:22 -0600

    Add files via upload

--- linux.sh
+++ linux.sh
@@ -1,1 +1,3 @@
+#abre leafpad.dekstop con editor de texto buscandolo en /usr/share/applications/ sino esta instala leafpad
+#para ver el argumento que envia la URL en leafpad.desktop en vez de usar %f usa %U
 xdg-mime default leafpad.desktop x-scheme-handler/appurl
